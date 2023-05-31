624c7daedc208aa7f553c3adc2d7c36f748f0db7
bpteam <bpteam22@gmail.com>
2023-06-01 00:36:44 +0300

    fix federation directive definition

---
+++ src/Utils/FederationV23SchemaExtender.php
@@ -0,0 +1,166 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Axtiva\FlexibleGraphql\Utils;
+
+use GraphQL\Language\AST\ArgumentNode;
+use GraphQL\Language\AST\DirectiveNode;
+use GraphQL\Language\AST\DocumentNode;
+use GraphQL\Language\AST\Node;
+use GraphQL\Language\AST\ObjectValueNode;
+use GraphQL\Language\AST\SchemaExtensionNode;
+use GraphQL\Language\AST\StringValueNode;
+use GraphQL\Language\Parser;
+use GraphQL\Type\Schema;
+use GraphQL\Utils\SchemaExtender;
+
+class FederationV23SchemaExtender extends FederationV1SchemaExtender
+{
+    private const SHAREABLE = 'directive @%s on OBJECT | FIELD_DEFINITION';
+    private const INACCESSIBLE = 'directive @%s on FIELD_DEFINITION | OBJECT | INTERFACE | UNION | ARGUMENT_DEFINITION | SCALAR | ENUM | ENUM_VALUE | INPUT_OBJECT | INPUT_FIELD_DEFINITION';
+    private const OVERRIDE = 'directive @%s(from: String!) on FIELD_DEFINITION';
+    private const TAG = 'directive @%s(name: String!) repeatable on | FIELD_DEFINITION | INTERFACE | OBJECT | UNION | ARGUMENT_DEFINITION | SCALAR | ENUM | ENUM_VALUE | INPUT_OBJECT | INPUT_FIELD_DEFINITION';
+    private const EXTERNAL = 'directive @%s on FIELD_DEFINITION';
+    private const REQUIRES = 'directive @%s(fields: FieldSet!) on FIELD_DEFINITION';
+    private const PROVIDES = 'directive @%s(fields: FieldSet!) on FIELD_DEFINITION';
+    private const KEY = 'directive @%s(fields: FieldSet!, resolvable: Boolean = true) repeatable on OBJECT | INTERFACE';
+    private const EXTENDS = 'directive @%s on OBJECT | INTERFACE';
+    private const COMPOSE_DIRECTIVE = 'directive @%s(name: String!) repeatable on SCHEMA';
+    private const INTERFACE_OBJECT = 'directive @%s on OBJECT | INTERFACE';
+
+    private const DIRECTIVE_MAP = [
+        'tag' => self::TAG,
+        'shareable' => self::SHAREABLE,
+        'inaccessible' => self::INACCESSIBLE,
+        'override' => self::OVERRIDE,
+        'external' => self::EXTERNAL,
+        'requires' => self::REQUIRES,
+        'provides' => self::PROVIDES,
+        'key' => self::KEY,
+        'extends' => self::EXTENDS,
+        'composeDirective' => self::COMPOSE_DIRECTIVE,
+        'interfaceObject' => self::INTERFACE_OBJECT,
+    ];
+
+    public static function build(Schema $schema, DocumentNode $ast): Schema
+    {
+        $schema = parent::build($schema, $ast);
+
+        $schema = self::addDirectiveIfNotExists(
+            $schema,
+            'link',
+            <<<GQL
+directive @link(
+  url: String, 
+  as: String, 
+  for: link__Purpose, 
+  import: [link__Import]
+) repeatable on SCHEMA
+enum link__Purpose {
+    "`SECURITY` features provide metadata necessary to securely resolve fields."
+    SECURITY
+    "`EXECUTION` features provide metadata necessary for operation execution."
+    EXECUTION
+}
+scalar link__Import
+GQL
+        );
+
+        foreach (static::DIRECTIVE_MAP as $directiveName => $directive) {
+            $schema = self::addDirectiveIfNotExists(
+                $schema,
+                $directiveName,
+                sprintf($directive, $directiveName)
+            );
+        }
+        $alias = 'federation';
+        foreach (static::DIRECTIVE_MAP as $directiveName => $directive) {
+            $schema = self::addDirectiveIfNotExists(
+                $schema,
+                $alias . '__' . $directiveName,
+                sprintf($directive, $alias . '__' . $directiveName)
+            );
+        }
+
+        $hasLinkExtension = false;
+        /** @var SchemaExtensionNode|Node $node */
+        foreach ($ast->definitions as $node) {
+            if ($node->kind === 'SchemaExtension') {
+                /** @var SchemaExtensionNode $node */
+                /** @var DirectiveNode $directive */
+                foreach ($node->directives->getIterator() as $directive) {
+                    if ($directive->name->value === 'link') {
+                        $hasLinkExtension = true;
+                        /** @var ArgumentNode $argument */
+                        foreach ($directive->arguments->getIterator() as $argument) {
+                            if ($argument->name->value === 'as') {
+                                $schema = self::addFederationDirectivesWithAliases(
+                                    $schema,
+                                    self::trimDirectivePrefix($argument->value->value),
+                                );
+                            } elseif ($argument->name->value === 'import') {
+                                /** @var ObjectValueNode|StringValueNode $value */
+                                foreach ($argument->value->values->getIterator() as $value) {
+                                    if ($value instanceof StringValueNode) {
+                                        $directiveName = self::trimDirectivePrefix($value->value);
+                                        if (empty(static::DIRECTIVE_MAP[$directiveName])) {
+                                            continue;
+                                        }
+                                        $schema = self::addDirectiveIfNotExists(
+                                            $schema,
+                                            $directiveName,
+                                            sprintf(static::DIRECTIVE_MAP[$directiveName], $directiveName)
+                                        );
+                                    } elseif ($value instanceof ObjectValueNode) {
+                                        $name = null;
+                                        $as = null;
+                                        /** @var ArgumentNode $argument */
+                                        foreach ($value->fields->getIterator() as $argument) {
+                                            if ($argument->name->value === 'name') {
+                                                $name = self::trimDirectivePrefix($argument->value->value);
+                                            }
+                                            if ($argument->name->value === 'as') {
+                                                $as = self::trimDirectivePrefix($argument->value->value);
+                                            }
+                                        }
+                                        if (empty(static::DIRECTIVE_MAP[$name])) {
+                                            continue;
+                                        }
+                                        if ($name !== null && $as !== null) {
+                                            $schema = self::addDirectiveIfNotExists(
+                                                $schema,
+                                                $as,
+                                                sprintf(static::DIRECTIVE_MAP[$name], $as)
+                                            );
+                                        } elseif ($name !== null) {
+                                            $schema = self::addDirectiveIfNotExists(
+                                                $schema,
+                                                $name,
+                                                sprintf(static::DIRECTIVE_MAP[$name], $name)
+                                            );
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!$hasLinkExtension) {
+            $documentAST = Parser::parse(
+                'extend schema @link(url: "https://specs.apollo.dev/federation/v2.3")'
+            );
+            $schema = SchemaExtender::extend($schema, $documentAST);
+        }
+
+        return $schema;
+    }
+
+    private static function trimDirectivePrefix(string $value): string
+    {
+        return ltrim($value, '@');
+    }
+}
---
+++ tests/Utils/FederationSchemaExtender/FederationV23SchemaExtenderCustomImportSchemaTest.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Axtiva\FlexibleGraphql\Tests\Utils\FederationSchemaExtender;
+
+use Axtiva\FlexibleGraphql\Utils\FederationV23SchemaExtender;
+use GraphQL\Error\SyntaxError;
+use GraphQL\Language\Parser;
+use GraphQL\Utils\BuildSchema;
+use PHPUnit\Framework\TestCase;
+
+class FederationV23SchemaExtenderCustomImportSchemaTest extends TestCase
+{
+    /**
+     * @return void
+     * @throws SyntaxError
+     */
+    public function testForGlobalAliasAs()
+    {
+        $sdl = <<<'SDL'
+scalar _FieldSet
+directive @link(
+  url: String, 
+  as: String, 
+  for: link__Purpose, 
+  import: [link__Import]
+) repeatable on SCHEMA
+enum link__Purpose {
+    "`SECURITY` features provide metadata necessary to securely resolve fields."
+    SECURITY
+    "`EXECUTION` features provide metadata necessary for operation execution."
+    EXECUTION
+}
+scalar link__Import
+directive @isAuthenticated on FIELD | FIELD_DEFINITION
+directive @hasRole(role: String) on FIELD | FIELD_DEFINITION
+directive @pow(ex: Int!) on FIELD | FIELD_DEFINITION
+directive @uppercase on FIELD | FIELD_DEFINITION
+
+extend schema @link(url: "http://localhost:8080/graphql", as: "@omg")
+
+type Query {
+  hero: Character
+}
+
+type Character {
+  name: String
+  friends: [Character]
+  homeWorld: Planet
+  species: Species
+}
+
+type Planet {
+  name: String
+  climate: String
+}
+
+type Species {
+  name: String
+  lifespan: Int
+  origin: Planet
+}
+SDL;
+
+        $ast = Parser::parse($sdl);
+        $schema = BuildSchema::build($ast);
+        $schemaExtended = FederationV23SchemaExtender::build($schema, $ast);
+
+        $this->assertFalse($schemaExtended->getQueryType()->hasField('_entities'), '_entity found');
+        $this->assertTrue($schemaExtended->getQueryType()->hasField('_service'), '_service found');
+        $this->assertNotNull($schemaExtended->getDirective('external'), 'external not found');
+        $this->assertNotNull($schemaExtended->getDirective('requires'), 'requires not found');
+        $this->assertNotNull($schemaExtended->getDirective('provides'), 'provides not found');
+        $this->assertNotNull($schemaExtended->getDirective('extends'), 'extends not found');
+        $this->assertNotNull($schemaExtended->getDirective('link'), 'link not found');
+        $this->assertNotNull($schemaExtended->getDirective('shareable'), 'shareable not found');
+        $this->assertNotNull($schemaExtended->getDirective('omg__shareable'), 'omg__shareable not found');
+        $this->assertNotNull($schemaExtended->getDirective('federation__shareable'), 'federation__shareable not found');
+        $this->assertNotNull($schemaExtended->getDirective('inaccessible'), 'inaccessible not found');
+        $this->assertNotNull($schemaExtended->getDirective('omg__inaccessible'), 'omg__inaccessible not found');
+        $this->assertNotNull($schemaExtended->getDirective('federation__inaccessible'), 'federation__inaccessible not found');
+        $this->assertNotNull($schemaExtended->getDirective('tag'), 'tag not found');
+        $this->assertNotNull($schemaExtended->getDirective('omg__tag'), 'omg__tag not found');
+        $this->assertNotNull($schemaExtended->getDirective('federation__tag'), 'federation__tag not found');
+        $this->assertNotNull($schemaExtended->getDirective('override'), 'override not found');
+        $this->assertNotNull($schemaExtended->getDirective('omg__override'), 'omg__override not found');
+        $this->assertNotNull($schemaExtended->getDirective('federation__override'), 'federation__override not found');
+    }
+
+    /**
+     * @return void
+     * @throws SyntaxError
+     */
+    public function testForAliasImport()
+    {
+        $sdl = <<<'SDL'
+scalar _FieldSet
+directive @link(
+  url: String, 
+  as: String, 
+  for: link__Purpose, 
+  import: [link__Import]
+) repeatable on SCHEMA
+enum link__Purpose {
+    "`SECURITY` features provide metadata necessary to securely resolve fields."
+    SECURITY
+    "`EXECUTION` features provide metadata necessary for operation execution."
+    EXECUTION
+}
+scalar link__Import
+directive @isAuthenticated on FIELD | FIELD_DEFINITION
+directive @hasRole(role: String) on FIELD | FIELD_DEFINITION
+directive @pow(ex: Int!) on FIELD | FIELD_DEFINITION
+directive @uppercase on FIELD | FIELD_DEFINITION
+
+extend schema @link(
+url: "http://localhost:8080/graphql", 
+import: [
+        { name: "@shareable", as: "@omgs" }, 
+        { name: "@inaccessible", as: "@iomg" }
+    ]
+)
+
+type Query {
+  hero: Character
+}
+
+type Character {
+  name: String
+  friends: [Character]
+  homeWorld: Planet
+  species: Species
+}
+
+type Planet {
+  name: String
+  climate: String
+}
+
+type Species {
+  name: String
+  lifespan: Int
+  origin: Planet
+}
+SDL;
+
+        $ast = Parser::parse($sdl);
+        $schema = BuildSchema::build($ast);
+        $schemaExtended = FederationV23SchemaExtender::build($schema, $ast);
+
+        $this->assertFalse($schemaExtended->getQueryType()->hasField('_entities'), '_entity found');
+        $this->assertTrue($schemaExtended->getQueryType()->hasField('_service'), '_service found');
+        $this->assertNotNull($schemaExtended->getDirective('external'), 'external not found');
+        $this->assertNotNull($schemaExtended->getDirective('requires'), 'requires not found');
+        $this->assertNotNull($schemaExtended->getDirective('provides'), 'provides not found');
+        $this->assertNotNull($schemaExtended->getDirective('extends'), 'extends not found');
+        $this->assertNotNull($schemaExtended->getDirective('link'), 'link not found');
+        $this->assertNotNull($schemaExtended->getDirective('shareable'), 'shareable not found');
+        $this->assertNotNull($schemaExtended->getDirective('omgs'), 'omgs not found');
+        $this->assertNotNull($schemaExtended->getDirective('federation__shareable'), 'federation__shareable not found');
+        $this->assertNotNull($schemaExtended->getDirective('inaccessible'), 'inaccessible not found');
+        $this->assertNotNull($schemaExtended->getDirective('iomg'), 'iomg not found');
+        $this->assertNotNull($schemaExtended->getDirective('federation__inaccessible'), 'federation__inaccessible not found');
+        $this->assertNotNull($schemaExtended->getDirective('tag'), 'tag not found');
+        $this->assertNotNull($schemaExtended->getDirective('federation__tag'), 'federation__tag not found');
+        $this->assertNotNull($schemaExtended->getDirective('override'), 'override not found');
+        $this->assertNotNull($schemaExtended->getDirective('federation__override'), 'federation__override not found');
+    }
+
+}
