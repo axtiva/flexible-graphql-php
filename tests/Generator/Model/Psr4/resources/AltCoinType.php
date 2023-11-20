b3c678b6cd9d1a740d239597da10ecf582680907
bpteam <bpteam22@gmail.com>
2023-11-20 13:56:33 +0200

    fix auto gernerator of resolver for not existed field in type model

--- tests/Generator/Model/Psr4/resources/AltCoinType.php
+++ tests/Generator/Model/Psr4/resources/AltCoinType.php
@@ -1,7 +1,7 @@
 <?php
 
 declare (strict_types=1);
-namespace Axtiva\FlexibleGraphql\Example\GraphQL\GraphQL\Model;
+namespace Axtiva\FlexibleGraphql\Example\GraphQL\Model;
 
 final class AltCoinType
 {
